f717e70daa33cb3f76909d23b7a8375c073d54f8
LuluLafuente <127445668+LuluLafuente@users.noreply.github.com>
2023-08-19 22:10:40 -0300

    Crear Mesa Final
    Administrador ingresa para seleccionar carrera, materia, fecha de examen y a los alumnos que rendiran guarda los datos en examen_final y en rinde para que despues desde el panel de docente cargar_examen pueda registrar a los alumnos presentes y sus notas, como docente solo debera seleccionar la carrera la materia que se rendira y el dia para asi poder filtrar a los alumnos que estan inscriptos a rendir

--- crear_mesa.php
+++ crear_mesa.php
@@ -3,28 +3,44 @@
 //INICIO SESION
 session_start();
 
-//AGREGO LAS FUNCIONES NECESARIAS PARA EL FUNCIONAMIENTO DE LA CARGA DE ALUMNOS
-include 'bd_conexion.php';
-include 'bd_select.php';
-
 //REVISO QUE HAYA UNA SESION ACTIVA DE USUARIO,
 //DE LO CONTRARIO VUELVE A LA PAGINA DE INICIO.
 if(!isset($_SESSION["usuario"])){
     //header("location:index.php");
     //exit;
 }
+//AGREGO LAS FUNCIONES NECESARIAS PARA EL FUNCIONAMIENTO DE LA CARGA DE ALUMNOS
+include 'bd_conexion.php';
+include 'bd_select.php';
 
 //CONEXION CON LA BASE DE DATOS
 $conexion = conexionDB();
 
 
+$query = "SELECT MAX(ID_EXAMEN_FINAL) AS num_mesa,
+MAX(LIBRO) AS num_libro, MAX(FOLIO) AS num_folio FROM examen_final";
+$statement = $conexion->prepare($query);
+$statement->execute();
+$resultado = $statement->fetch(PDO::FETCH_ASSOC);
+
+$num_mesa = $resultado['num_mesa'];
+$num_libro = $resultado['num_libro'];
+$num_folio = $resultado['num_folio'];
+
+
+
 //Busqueda de carrera y materias
 $consulta1 = selectCarrera($conexion);
 $consulta2 = selectMaterias($conexion);
+$consulta3 = selectAlumnosCursado($conexion);
+
 
 //GUARDO TODOS LOS RESULTADOS EN UNA VARIABLE
 $resultado1 = $consulta1->fetchAll();
 $resultado2 = $consulta2->fetchAll();
+$resultado3 = $consulta3->fetchAll();
+
+
 
 
 include('crear_mesa.html');
